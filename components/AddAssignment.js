6f339cc31812b34669ea98676aa641cfd25b986a
ciper <20215816@e.ubaguio.edu>
2023-09-14 09:09:00 +0800

    feat: added add/view/delete/mark as done assignment functionality

--- components/AddAssignment.js
+++ components/AddAssignment.js
@@ -1,167 +1,169 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Pressable, StyleSheet, TextInput, Modal } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 const AddAssignment = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { assignments, setAssignments } = route.params;
 
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [newAssignment, setNewAssignment] = useState('');
 
-    const goBack = () => {
-        navigation.goBack(); // Navigates back to the previous screen
-    };
+  const goBack = () => {
+    navigation.goBack();
+  };
 
-    const openModal = () => {
-        setIsModalVisible(true);
-    };
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
 
-    const closeModal = () => {
-        setIsModalVisible(false);
-    };
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.containerForm}>
-                <View style={styles.backAndAddHeader}>
-                    <Pressable style={styles.backBtn} onPress={goBack}>
-                        <Text style={styles.backBtnText}>Back</Text>
-                    </Pressable>
-                    <Text>ADD AN ASSIGNMENT</Text>
-                </View>
-                {/* START OF TEXT INPUT */}
-                <View style={styles.bodyForm}>
-                    <Text style={styles.titleText}>Title</Text>
-                    <TextInput style={styles.inputTitle} maxLength={50} />
-                    <Text style={styles.descriptionTitle}> Description</Text>
-                    <TextInput
-                        multiline
-                        style={styles.inputDescription}
-                        numberOfLines={5}
-                        maxLength={200}
-                    />
-                    <Pressable style={styles.addAssignmentBtn} onPress={openModal}>
-                        <Text style={styles.addAssignmentText}>
-                            Add Assignment
-                        </Text>
-                    </Pressable>
-                </View>
-                {/* END OF TEXT INPUT */}
-            </View>
-            <Modal
-                visible={isModalVisible}
-                animationType="slide"
-                transparent={true}
-                onRequestClose={closeModal}
-            >
-                {/* MODAL CONTENT */}
-                <View style={styles.modalContainer}>
-                    <Text style={styles.modalText}>Assignment Added!</Text>
-                    <Pressable style={styles.closeModalBtn} onPress={closeModal}>
-                        <Text style={styles.closeModalText}>Close</Text>
-                    </Pressable>
-                </View>
-            </Modal>
+  const handleAddAssignment = () => {
+    if (newAssignment.trim() !== '') {
+      setAssignments([...assignments, { title: newAssignment, done: false }]);
+      setNewAssignment('');
+    }
+  };
+
+  useEffect(() => {
+    // This effect will close the modal when a new assignment is added.
+    if (isModalVisible) {
+      closeModal();
+    }
+  }, [assignments]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerForm}>
+        <View style={styles.backAndAddHeader}>
+          <Pressable style={styles.backBtn} onPress={goBack}>
+            <Text style={styles.backBtnText}>Back</Text>
+          </Pressable>
+          <Text>ADD AN ASSIGNMENT</Text>
         </View>
-    );
+        <View style={styles.bodyForm}>
+          <Text style={styles.titleText}>Title</Text>
+          <TextInput
+            style={styles.inputTitle}
+            maxLength={50}
+            value={newAssignment}
+            onChangeText={(text) => setNewAssignment(text)}
+          />
+          <Pressable style={styles.addAssignmentBtn} onPress={handleAddAssignment}>
+            <Text style={styles.addAssignmentText}>Add Assignment</Text>
+          </Pressable>
+        </View>
+      </View>
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalText}>Assignment Added!</Text>
+          <Pressable style={styles.closeModalBtn} onPress={closeModal}>
+            <Text style={styles.closeModalText}>Close</Text>
+          </Pressable>
+        </View>
+      </Modal>
+    </View>
+  );
 };
 
 export default AddAssignment;
 
 const styles = StyleSheet.create({
-   
-    container: {
-        height: '100%',
-    },
-    containerForm: {
-        backgroundColor: '#FFF',
-        height: '95%',
-        width: '90%',
-        position: 'absolute',
-        marginLeft: 20,
-        marginTop: 20,
-        borderRadius: 20,
-
-    },
-    backAndAddHeader: {
-        height: '25%',
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingTop: 20,
-    },
-    backBtn: {
-        backgroundColor: '#3546A0',
-        height: 20,
-        width: 40,
-    },
-    backBtnText: {
-
-    },
-    bodyForm: {
-        height: '75%',
-        paddingLeft: 20,
-    },
-    titleText: {
-        fontSize: 10,
-        fontWeight: 'bold',
-        paddingBottom: 5,
-
-    },
-    inputTitle: {
-        backgroundColor: '#ECECEC',
-        marginRight: 20,
-        borderRadius: 5,
-        height: 25,
-    },
-    descriptionTitle: {
-        fontSize: 10,
-        fontWeight: 'bold',
-        paddingTop: 10,
-        paddingBottom: 5,
-    },
-    inputDescription:{
-        backgroundColor: '#ECECEC',
-        marginRight: 20,
-        borderRadius: 5,
-    },
-    addAssignmentBtn: {
-        position: 'absolute',
-        marginTop: '60%',
-        marginLeft: '20%',
-        backgroundColor: '#3546A0',
-        width: 160,
-        height: 20,
-        borderRadius: 40,
-
-    },
-    addAssignmentText: {
-        paddingLeft: '28%',
-        paddingTop: '2%',
-        fontSize: 10,
-        color: 'white',
-    },
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    },
-    modalText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white',
-        marginBottom: 20,
-    },
-    closeModalBtn: {
-        backgroundColor: '#3546A0',
-        padding: 10,
-        borderRadius: 5,
-    },
-    closeModalText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
+  container: {
+    height: "100%",
+  },
+  containerForm: {
+    backgroundColor: "#FFF",
+    height: "95%",
+    width: "90%",
+    position: "absolute",
+    marginLeft: 20,
+    marginTop: 20,
+    borderRadius: 20,
+  },
+  backAndAddHeader: {
+    height: "25%",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 20,
+  },
+  backBtn: {
+    backgroundColor: "#3546A0",
+    height: 20,
+    width: 40,
+  },
+  backBtnText: {},
+  bodyForm: {
+    height: "75%",
+    paddingLeft: 20,
+  },
+  titleText: {
+    fontSize: 10,
+    fontWeight: "bold",
+    paddingBottom: 5,
+  },
+  inputTitle: {
+    backgroundColor: "#ECECEC",
+    marginRight: 20,
+    borderRadius: 5,
+    height: 25,
+  },
+  descriptionTitle: {
+    fontSize: 10,
+    fontWeight: "bold",
+    paddingTop: 10,
+    paddingBottom: 5,
+  },
+  inputDescription: {
+    backgroundColor: "#ECECEC",
+    marginRight: 20,
+    borderRadius: 5,
+  },
+  addAssignmentBtn: {
+    position: "absolute",
+    marginTop: "60%",
+    marginLeft: "20%",
+    backgroundColor: "#3546A0",
+    width: 160,
+    height: 20,
+    borderRadius: 40,
+  },
+  addAssignmentText: {
+    paddingLeft: "28%",
+    paddingTop: "2%",
+    fontSize: 10,
+    color: "white",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+  },
+  modalText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 20,
+  },
+  closeModalBtn: {
+    backgroundColor: "#3546A0",
+    padding: 10,
+    borderRadius: 5,
+  },
+  closeModalText: {
+    color: "white",
+    fontWeight: "bold",
+  },
 });
-
-
--- components/Home.js
+++ components/Home.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import { Text, View, Pressable, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Pressable, StyleSheet, TextInput, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const AddAssign = () => {
-        navigation.navigate('AddAssignment');
-    };
+  const [assignments, setAssignments] = useState([]);
+  const [newAssignment, setNewAssignment] = useState('');
+
+  const handleAddAssignment = () => {
+    if (newAssignment.trim() !== '') {
+      setAssignments([...assignments, { title: newAssignment, done: false }]);
+      setNewAssignment('');
+    }
+  };
+
+  const handleToggleAssignment = (index) => {
+    const updatedAssignments = [...assignments];
+    updatedAssignments[index].done = !updatedAssignments[index].done;
+    setAssignments(updatedAssignments);
+  };
+
+  const handleDeleteAssignment = (index) => {
+    if (!assignments[index].done) {
+      const updatedAssignments = [...assignments];
+      updatedAssignments.splice(index, 1);
+      setAssignments(updatedAssignments);
+    }
+  };
+
+  const AddAssign = () => {
+    navigation.navigate('AddAssignment', { assignments, setAssignments });
+  };
 
   return (
     <View style={styles.pageSizingContainer}>
-      {/* Start of top divider */}
       <View style={styles.pageHeader}>
         <Text style={styles.headerGreeting}>Hi there,</Text>
         <Text style={styles.headerTitle}>Check on all your Assignments</Text>
       </View>
-      {/* End of divider */}
       <View style={styles.pageBody}>
-        {/* Start of Active and Archived Buttons */}
         <View style={styles.activeAndArchived}>
           <Pressable style={styles.buttonActive}>
             <Text style={styles.aButtonProps}>ACTIVE</Text>
@@ -27,20 +48,32 @@ const Home = () => {
             <Text style={styles.aButtonProps}>ARCHIVED</Text>
           </Pressable>
         </View>
-        {/* End of Active and Archived Button */}
-        {/* Start of list items */}
-        <View style={styles.items}>
-          {/* Items go here */}
-          <Text>Item 1</Text>
-        </View>
-        {/* End of list items */}
-        {/* Start of Add Button */}
+        <ScrollView style={styles.items}>
+          {assignments.map((assignment, index) => (
+            <View key={index} style={styles.assignmentItem}>
+              <Text>{assignment.title}</Text>
+              <Pressable
+                style={[styles.doneButton, { backgroundColor: assignment.done ? 'green' : 'red' }]}
+                onPress={() => handleToggleAssignment(index)}
+              >
+                <Text style={styles.doneButtonText}>{assignment.done ? 'Done' : 'Not Done'}</Text>
+              </Pressable>
+              {!assignment.done && (
+                <Pressable
+                  style={styles.deleteButton}
+                  onPress={() => handleDeleteAssignment(index)}
+                >
+                  <Text style={styles.deleteButtonText}>Delete</Text>
+                </Pressable>
+              )}
+            </View>
+          ))}
+        </ScrollView>
         <View style={styles.bottomAddButton}>
           <Pressable style={styles.bottomButton} onPress={AddAssign}>
             <Text style={styles.bottomText}>+</Text>
           </Pressable>
         </View>
-        {/* End of Add Button */}
       </View>
     </View>
   );
@@ -49,90 +82,84 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '808080',
-    
+    backgroundColor: "808080",
   },
   pageSizingContainer: {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
   },
   pageHeader: {
-    height: '30%',
-    width: '100%',
-    backgroundColor: '#3546A0',
-    paddingTop: '30%',
-    paddingLeft: '10%',
-    
+    height: "30%",
+    width: "100%",
+    backgroundColor: "#3546A0",
+    paddingTop: "30%",
+    paddingLeft: "10%",
   },
-  headerGreeting:{
-    color: '#FFF',
+  headerGreeting: {
+    color: "#FFF",
     opacity: 0.5,
-    
   },
-  headerTitle:{
+  headerTitle: {
     fontSize: 30,
-    color: '#FFF',
-    fontWeight: 'bold',
-    fontFamily: 'arial',
-    width: '80%',
-    
+    color: "#FFF",
+    fontWeight: "bold",
+    fontFamily: "arial",
+    width: "80%",
   },
   // All Body Elements Below
   pageBody: {
-    height: '70%',
-    backgroundColor: 'FFF',
+    height: "70%",
+    backgroundColor: "FFF",
   },
-  activeAndArchived:{
-    paddingTop: '10%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingLeft: '20%',
-    paddingRight: '20%',
-    
+  activeAndArchived: {
+    paddingTop: "10%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: "20%",
+    paddingRight: "20%",
   },
-  buttonActive:{
-    backgroundColor: '#808080',
+  buttonActive: {
+    backgroundColor: "#808080",
     borderRadius: 50,
-    width: '23%',
-    height: '120%',
+    width: "23%",
+    height: "120%",
   },
-  buttonArchived:{
-    backgroundColor: '#808080',
+  buttonArchived: {
+    backgroundColor: "#808080",
     borderRadius: 50,
-    width: '30%',
-    height: '120%',
+    width: "30%",
+    height: "120%",
   },
-  aButtonProps:{
-    fontWeight: 'bold',
+  aButtonProps: {
+    fontWeight: "bold",
     fontSize: 12,
-    paddingLeft: '10%',
+    paddingLeft: "10%",
   },
   items: {
     paddingLeft: 25,
     paddingTop: 15,
   },
   bottomAddButton: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 30,
-    width: '100%',
-    paddingLeft: '80%',
-    paddingRight: '5%',
+    width: "100%",
+    paddingLeft: "80%",
+    paddingRight: "5%",
   },
-  bottomButton:{
+  bottomButton: {
     width: 60,
     height: 60,
-    backgroundColor: '#3546A0',
+    backgroundColor: "#3546A0",
     borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
     borderWidth: 1,
     paddingBottom: 10,
   },
   bottomText: {
     fontSize: 35,
   },
-
 });
 
 export default Home;
